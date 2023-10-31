dd1646b8c4e2620af8d44f6e6c72c11ee387868a
Oduigwe Gerald <paulsongerald00@gmail.com>
2023-10-31 01:53:21 +0100

    worked on the employee REST API by using mongoose to connect to mongoDB to allow CRUD operations

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,71 +1,125 @@
 const Employee = require("../model/Employee");
 
-const getAllEmployees = (req, res) => {
-  console.log("req header", req.headers); //
-  console.log("res header", res.header()); //
-  res.json(data.employees);
+const getAllEmployees = async (req, res) => {
+  // console.log("req header", req.headers); //
+  // console.log("res header", res.header()); //
+  // res.json(data.employees);
+
+  // using mongoose
+  const employees = await Employee.find();
+  if (!employees)
+    return res
+      .status(204)
+      .json({ message: "no employees found in this end point" });
+  res.json(employees);
 };
 
 const RegisterNewEmployee = async (req, res) => {
   const { firstname, lastname } = req.body;
   if (!firstname || !lastname) {
-    res.status(403).json({ message: "required user details" });
+    res.status(400).json({ message: "required user details" });
   }
-  const newEm = {
-    firstname,
-    lastname,
-    id: data.employees[data.employees.length - 1].id + 1 || 1,
-  };
-
-  const result = await Employee.create({ newEm });
-  console.log(result);
-
-  res.status(201).json(data.employees);
+  try {
+    const result = await Employee.create({ firstname, lastname });
+    res.status(201).json(result);
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
-const updateEmployee = (req, res) => {
-  const { firstname, lastname, id } = req.body;
-
-  const curEmpl = data.employees.find(
-    (employee) => employee.id === parseInt(id)
-  );
-  if (!curEmpl)
-    res
+const updateEmployee = async (req, res) => {
+  const { id } = req?.body;
+  if (!id)
+    return res
       .status(400)
-      .json({ message: `employee with id ${id} seems not to exist` });
-  if (firstname) curEmpl.firstname = firstname;
-  if (lastname) curEmpl.lastname = lastname;
-  const filteredArray = data.employees.filter(
-    (employees) => employees.id !== parseInt(id)
-  );
-  const unsortedArray = [...filteredArray, curEmpl];
-  const sortedArray = unsortedArray.sort((a, b) => a.id - b.id);
-  data.setEmployee(sortedArray);
-  res.json(data.employees);
+      .json({ message: "ID parameter is required for this action" });
+
+  const employee = await Employee.findOne({ _id: id }).exec();
+
+  if (!employee)
+    return res
+      .json(400)
+      .json({ message: `no employee found with the given id ${id}` });
+  if (req?.body?.firstname) employee.firstname = req.body.firstname;
+  if (req?.body?.lastname) employee.lastname = req.body.lastname;
+
+  const result = await employee.save();
+  res.json(result);
+
+  //BELOW IS  CODE WITHOUT MONGODB AND MONGOOSE
+
+  // const { firstname, lastname, id } = req.body;
+
+  // const curEmpl = await Employee.findOne({ _id }).exec();
+  // if (!curEmpl)
+  //   res
+  //     .status(400)
+  //     .json({ message: `employee with id ${id} seems not to exist` });
+  // if (firstname) curEmpl.firstname = firstname;
+  // if (lastname) curEmpl.lastname = lastname;
+  // const filteredArray = data.employees.filter(
+  //   (employees) => employees.id !== parseInt(id)
+  // );
+  // const unsortedArray = [...filteredArray, curEmpl];
+  // const sortedArray = unsortedArray.sort((a, b) => a.id - b.id);
+  // data.setEmployee(sortedArray);
+  // res.json(data.employees);
 };
 
-const deleteEmployee = (req, res) => {
-  const employee = data.employees.find((em) => em.id === parseInt(req.body.id));
+const deleteEmployee = async (req, res) => {
+  const { id } = req?.body;
+  if (!id)
+    return res
+      .status(400)
+      .json({ message: "ID parameter is required for this action" });
+
+  const employee = await Employee.findOne({ _id: id }).exec();
   if (!employee)
-    res.status(400).json({
-      message: `please gerry, this user with id ${req.body.id} does not exist`,
-    });
-  const newEmployees = data.employees.filter(
-    (employee) => employee.id !== parseInt(req.body.id)
-  );
-  data.setEmployee(newEmployees);
-  res.json(data.employees);
+    return res
+      .json(400)
+      .json({ message: `no employee found with the given id ${id}` });
+
+  const result = await employee.deleteOne({ _id: id });
+  res.json(result);
+
+  //BELOW IS  CODE WITHOUT MONGODB AND MONGOOSE
+
+  // const employee = data.employees.find((em) => em.id === parseInt(req.body.id));
+  // if (!employee)
+  //   res.status(400).json({
+  //     message: `please gerry, this user with id ${req.body.id} does not exist`,
+  //   });
+  // const newEmployees = data.employees.filter(
+  //   (employee) => employee.id !== parseInt(req.body.id)
+  // );
+  // data.setEmployee(newEmployees);
+  // res.json(data.employees);
 };
 
-const getEmployee = (req, res) => {
-  const employee = data.employees.find(
-    (em) => em.id === parseInt(req.params.id)
-  );
+const getEmployee = async (req, res) => {
+  const { id } = req?.params;
+  if (!id)
+    return res
+      .status(400)
+      .json({ message: "ID parameter is required for this action" });
+  const employee = await Employee.findOne({ _id: id }).exec();
   if (!employee)
-    res.status(400).json({
-      message: `please gerry ➡⬅😥, this user with id ${req.body.id} does not exist`,
-    });
+    return res
+      .status(400)
+      .json({ message: `employee with id ${id} does not seem to exist` });
   res.json(employee);
+
+  //BELOW IS  CODE WITHOUT MONGODB AND MONGOOSE
+
+  // const employee = data.employees.find(
+  //   (em) => em.id === parseInt(req.params.id)
+  // );
+  // if (!employee)
+  //   res.status(400).json({
+  //     message: `please gerry ➡⬅😥, this user with id ${req.body.id} does not exist`,
+  //   });
+  // res.json(employee);
 };
 
 // error catcher
